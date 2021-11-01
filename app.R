8144239e4f7197892014c8379341bbb11cde989a
Thompson <Elizabeth.Thompson@dfo-mpo.gc.ca>
2021-11-01 10:29:10 -0300

    add trellis and 2541 new gulf whales

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 #################################
 ## NARW Head and Tails App     ##
 ## By: Liz Thompson            ##  
-## Last updated: Oct 14, 2021 ##
+## Last updated: Nov 1, 2021 ##
 #################################
 
 library(shinydashboard)
