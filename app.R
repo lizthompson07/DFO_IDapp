25fb13390f08fcc86f9254162cd682fe55331a9f
lizthompson07 <lizthompson07@gmail.com>
2022-11-16 17:41:43 -0400

    added 3693 to glfwhales

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 #################################
 ## NARW Head and Tails App     ##
 ## By: Liz Thompson            ##  
-## Last updated: Oct 26, 2022   ##
+## Last updated: Nov 16, 2022  ##
 #################################
 
 library(shinydashboard)
