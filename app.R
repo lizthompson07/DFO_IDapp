b132c9de60c04ba9a4eafe0f3c9f83349aeca99e
lizthompson07 <lizthompson07@gmail.com>
2022-12-07 11:46:27 -0400

    update

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
 #################################
 ## NARW Head and Tails App     ##
 ## By: Liz Thompson            ##  
-## Last updated: Nov 16, 2022  ##
+## Last updated: Dec 7, 2022  ##
 #################################
 
 library(shinydashboard)
